ffadee49a257155ddb97b172e15ea320e011fedd
Paolo Čerić <paolo.ceric@microblink.com>
2025-08-07 15:29:38 +0200

    chore: vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [vue()],
   publicDir: 'public',
-  base: './',
+  base: './dist',
   build: {
     assetsDir: 'assets',
     rollupOptions: {
